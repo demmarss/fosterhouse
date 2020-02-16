b48787eef53030e0de4812fb6eee2e3fe2860660
demmarss <demmarss@gmail.com>
2020-02-16 07:24:20 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from '../src/components/header'
 import Home from './components/home';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div >
       <Header/>
       <Home/>
-
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Footer(){
+    return <footer class="footer">
+    <div class="content container">
+        <div className='columns'>
+            <div className='column'>
+                <p className='title is-4'>Address</p>
+                <p>
+            1318 32nd Street Circle South
+        </p>
+        <p>
+            Moorhead, Minnesota 56560
+        </p>
+        <p>218-303-2185, 609-287-0169
+        </p>
+        <p>
+            adultcare@socialworker.net, caringforadult@gmail.com
+        </p>
+            </div>
+            <div className='column'>
+            <p className='title is-4'>Links</p>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>LinkedIn</p>
+            </div>
+            <div className='column'>
+            <p className='title is-4'>More Information</p>
+            </div>
+
+        </div>
+      
+    </div>
+  </footer>
+}
